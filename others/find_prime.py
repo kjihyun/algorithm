9f6b5a14cc4ea691b79c6f89284cbf50c491b5af
jihyun <jihyun7739@gmail.com>
2021-02-02 15:12:25 +0900

    other algorithm

---
+++ others/find_prime.py
@@ -0,0 +1,16 @@
+input = 20
+
+
+def find_prime_list_under_number(number):
+    prime_list=[]
+    for i in range(2, number + 1):  ## 2~20
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            prime_list.append(i)
+    return prime_list
+
+
+result = find_prime_list_under_number(input)
+print(result)
