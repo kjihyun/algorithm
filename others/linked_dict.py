2721b3be6724295aeadc4278adfca0b391ecc322
jihyun <jihyun7739@gmail.com>
2021-02-16 17:38:39 +0900

    add linked dict

---
+++ others/linked_dict.py
@@ -0,0 +1,13 @@
+class LinkedTuple:
+    def __init__(self):
+        self.items=list()
+
+    def add(self,key,value):
+
+
+
+class LinkedDict :
+    def __init__(self):
+        self.items=[]
+        for i in range(8) :
+            self.items.append(LinkedTuple())
